9885f8e9ab4762e82ddead05faf5a7e14761f782
xckou <35790746+xckou@users.noreply.github.com>
2018-02-06 14:20:48 -0500

    add secrets

---
+++ le10_ex/secrets.py
@@ -0,0 +1,4 @@
+client_key = 'HTXSBuak9zCM7VtobltESkjGZ'
+client_secret = 'ZnMCu18TJhhYRUtXkQVfYaEj3WQAoD3e5rgXSO7rudBA3vUBHV'
+access_token = '959133807529119747-qzrIOzDc1TyzVjRVMagKXuyUNGldZRB'
+access_token_secret = 'hfbH24pqZj2A13bJFIzX2LSOaaefqjOCWoT5Pzc1UCk9n'
